2c32aef5ad317b465d1d289c9afdc314e399b34c
Cai <106552747+aoxijy@users.noreply.github.com>
2025-05-31 02:07:12 +0800

    Update diy2.sh

--- immortalwrt-24/diy2.sh
+++ immortalwrt-24/diy2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改默认IP地址
-sed -i "s/192.168.1.1/192.168.3.1/g" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/172.18.18.222/g" package/base-files/files/bin/config_generate
 # 修改设备说明
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
 # OpenClash
