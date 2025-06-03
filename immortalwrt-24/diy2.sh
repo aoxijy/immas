5c84aae073bf5da01ce76c3d831426ee59f5db91
Cai <106552747+aoxijy@users.noreply.github.com>
2025-06-03 21:00:44 +0800

    Update diy2.sh

--- immortalwrt-24/diy2.sh
+++ immortalwrt-24/diy2.sh
@@ -11,7 +11,7 @@
 # 去掉 gn 包所有 Makefile 中的 -Werror
 find feeds/packages/devel/gn -type f -exec sed -i 's/-Werror//g' {} +
 # 进入 gn 包目录，修改 Makefile 或编译参数，去掉 -Werror
-sed -i 's/-Werror//g' feeds/packages/devel/gn/Makefile
+
 # 修改默认IP地址
 sed -i "s/192.168.1.1/172.18.18.222/g" package/base-files/files/bin/config_generate
 # 修改设备说明
