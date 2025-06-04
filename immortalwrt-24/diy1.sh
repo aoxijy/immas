5e165bda093fe39e772f1590ffb7d9fb5b585488
Cai <106552747+aoxijy@users.noreply.github.com>
2025-06-04 12:29:42 +0800

    Update diy1.sh

--- immortalwrt-24/diy1.sh
+++ immortalwrt-24/diy1.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 删除旧 golang，准备替换
-rm -rf feeds/packages/lang/golang
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 添加第三方软件源
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-# 克隆 Easytier
+# 自定义软件源
+git clone --depth 1 -b master https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone --depth 1 -b master https://github.com/kenzok8/small.git package/openwrt-packages
+# Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
